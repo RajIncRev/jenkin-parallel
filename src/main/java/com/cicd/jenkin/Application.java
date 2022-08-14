5a805287ac68e05bd558003cee87f23005a19f4f
RajIncRev <rajkishore.behura@gmail.com>
2022-08-14 11:23:47 +0530

    Added buid command

--- src/main/java/com/cicd/jenkin/Application.java
+++ src/main/java/com/cicd/jenkin/Application.java
@@ -8,14 +8,14 @@ public class Application {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
-		try {
+	/*	try {
 			int i=0;
 			if(i==0) {
 				throw new Exception();
 			}
 		}catch(Exception ex) {
 			throw ex;
-		}
+		}*/
 	}
 
 }
