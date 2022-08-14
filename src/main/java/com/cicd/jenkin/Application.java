bce275b9533f29e3cfaa10e6517a66e260e8c3aa
RajIncRev <rajkishore.behura@gmail.com>
2022-08-14 08:32:09 +0530

    Added exception

--- src/main/java/com/cicd/jenkin/Application.java
+++ src/main/java/com/cicd/jenkin/Application.java
@@ -6,8 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
+		try {
+			int i=0;
+			if(i==0) {
+				throw new Exception();
+			}
+		}catch(Exception ex) {
+			throw ex;
+		}
 	}
 
 }
